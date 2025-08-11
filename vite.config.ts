43da4c61bd9c8250132984abcf109fd5a52b3d24
Pushkar Kumar Sah <pushkarcdn@gmail.com>
2025-08-11 10:26:54 +0545

    Updated vite.config.ts to allow all hosts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   preview: {
-		allowedHosts: ['prime-skills.pushkar.live']
+		allowedHosts: true
 	}
 });
